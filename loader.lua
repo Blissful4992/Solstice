ba46fba1593691331786846f2e87d25ee0f10565
Blissful4992 <romainbesson8@gmail.com>
2023-06-11 01:32:04 +0200

    Update Loader and start Universal

--- loader.lua
+++ loader.lua
@@ -6,6 +6,7 @@ end
 
 -- Utilities
 util = loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/Miscellaneous/main/util.lua"))();
+library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/SimpleCSGO/main/src.lua"))();
 notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/Miscellaneous/main/notificationsJxereas.lua"))();
 
 -- Wait for game
@@ -33,16 +34,45 @@ for Id, Game in next, Games do
     end
 end
 
--- Find supported game
-local Supported = Games[game.GameId] ~= nil;
-if not Supported then
-    warn("> Solstice : Game isn't supported, loading Universal...");
-    notification.new("info", "Information", "Loading Universal...", true);
+-- Create the Window
+Window = library.new({
+    Size = V2(400, 500),
+    Position = V2(200, 200)
+})
 
+-- Basic game info
+Supported = Games[game.GameId] ~= nil;
+GameName = Supported and Games[game.GameId] or "Universal";
+GameNameLowered = GameName:lower();
+GameNameClean = GameName:gsub("_", " ");
 
-else
-    GameName = Games[game.GameId]:gsub("_", " ");
+-- Settings
+SettingsFile = "Solstice/" .. GameNameLowered .. ".dat";
 
-    warn("> Solstice : Loading " .. GameName .. "...")
-    notification.new("info", "Information", "Loading " .. GameName .. "...", true);
+function Load() -- LOAD SETTINGS (can be forked into a multiple config system)
+    local Result = readfile(SettingsFile) or "{}";
+    local Table = HttpService:JSONDecode(Result) or {};
+
+    Settings = util.merge(Settings or {}, Table);
 end
+
+function Save() -- SAVE SETTINGS
+    writefile(SettingsFile, HttpService:JSONEncode(Settings or {}) or "{}");
+end
+
+function SaveDefault() -- SAVE SETTINGS
+    if not isfile(SettingsFile) then
+        writefile(SettingsFile, HttpService:JSONEncode(Settings or {}) or "{}");
+    end
+end
+
+-- Execute scripts + Inform player
+notification.new("info", "Information", "Loading " .. GameNameClean);
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/Solstice/main/scripts/" .. GameNameClean))();
+notification.new("success", "Success", "Loaded " .. GameNameClean .. " !");
+
+-- Window Theme
+Window:newThemeControlPage({
+    Folder = "SolsticeTheme",
+    Default = "Theme",
+})
