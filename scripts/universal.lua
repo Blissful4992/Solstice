047604d960ee1e12ecc808dabca2a84b1411b803
Blissful4992 <ezgamereally@gmail.com>
2023-08-22 20:52:37 +0200

    Update universal.lua

--- scripts/universal.lua
+++ scripts/universal.lua
@@ -841,8 +841,8 @@ if AIMBOT then
             end
             aim_c_1 = nil;
             Aiming = false;
-        elseif Settings.Aimbot.Aim_Mode == "Key" and input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode[Settings.Aimbot.Aim_Key] then
-            Aiming = true
+        -- elseif Settings.Aimbot.Aim_Mode == "Key" and input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode[Settings.Aimbot.Aim_Key] then
+        --     Aiming = true
         elseif Settings.Aimbot.Aim_Mode == "Mouse" and input.UserInputType == Enum.UserInputType.MouseButton2 then
             Aiming = true
         end
@@ -855,8 +855,8 @@ if AIMBOT then
             end
             aim_c_2 = nil;
             Aiming = false;
-        elseif Settings.Aimbot.Aim_Mode == "Key" and input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode[Settings.Aimbot.Aim_Key] then
-            Aiming = false
+        -- elseif Settings.Aimbot.Aim_Mode == "Key" and input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode[Settings.Aimbot.Aim_Key] then
+        --     Aiming = false
         elseif Settings.Aimbot.Aim_Mode == "Mouse" and input.UserInputType == Enum.UserInputType.MouseButton2 then
             Aiming = false
         end
